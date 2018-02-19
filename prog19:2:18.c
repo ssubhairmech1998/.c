1df68f86f6497b7c00306abfc7eac6a07fa495d5
ssubhairmech1998 <35688062+ssubhairmech1998@users.noreply.github.com>
2018-02-19 15:48:44 +0530

    prog19:2:18.c

---
+++ prog19:2:18.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int d,e,f;
+printf("Enter a value \n");
+scanf("%d",&d);
+printf(" Enter a value \n");
+scanf("%d",&e);
+c=pow(d,e);
+printf("%d \n",f);
+getch();
+return 0;
+}
