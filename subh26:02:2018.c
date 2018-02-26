8c1e41a8094932028aa792886f0949dd2c80daea
ssubhairmech1998 <35688062+ssubhairmech1998@users.noreply.github.com>
2018-02-26 10:23:26 +0000

    subh26:02:2018.c

---
+++ subh26:02:2018.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+ int n,count=0,rem,sum=0,temp1,temp2;
+ scanf("%d",&n);
+ temp1=temp2=n;
+ while(temp1>0)
+ {
+   
+     count++;
+     temp1/=10;
+ }
+ while(temp2>0)
+ {
+     rem=temp2%10;
+     sum=sum+pow(rem,count);
+     temp2/=10;
+ }
+ if(sum==n)
+ {
+     printf("yes\n");
+ }
+ else
+ {
+     printf("No");
+ }
+return 0;
+}
