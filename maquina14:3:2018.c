3c529c461851a187bea3653afbe2f26bec039255
ssubhairmech1998 <35688062+ssubhairmech1998@users.noreply.github.com>
2018-03-14 16:00:41 +0530

    maquina14:3:2018.c

---
+++ maquina14:3:2018.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char str[100];
+	int counter,count=0,countSpace;
+	for(counter=0;str[counter]!='\0';counter++)
+	{
+	if((str[counter]>='0' && str[counter]<='9')
+	|| (str[counter]>='A' && str[counter]<='Z')
+	|| (str[counter]>='a' && str[counter]<='z')
+          || (str[counter]==' ' || str[counter]== '.'))
+            countSpace++;
+          else
+            count++;
+	}
+	printf("%d",count);
+getch();
+return 0;
+}
